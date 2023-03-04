d4a6e3a8611a49599f120e124f611af2e35c051b
Francisco Arantes <Francisco.arantes.16864@gmail.com>
2023-03-04 16:12:36 +0000

    add main

---
+++ main.c
@@ -0,0 +1,15 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+int main()
+{
+    
+}
