1733b0f4b2acb20f56973882550385483a4695e9
Francisco Arantes <Francisco.arantes.16864@gmail.com>
2023-03-13 12:36:20 +0000

    First test and adding more rows to the menu

--- main.c
+++ main.c
@@ -15,6 +15,16 @@ int menu()
  printf("7. Read Client\n");
  printf("8. Read Vehicle\n");
  printf("9. Read Manager\n");
+ printf("10. Remove Client\n");
+ printf("11. Remove Vehicle\n");
+ printf("12. Remove Manager\n");
+ printf("13. Change Client\n");
+ printf("14. Change Vehicle\n");
+ printf("15. Change Manager\n");
+ printf("16. Register Vehicle\n");
+ printf("17. List Vehicles\n");
+ printf("18. List Vehicles per Location\n");
+ printf("0. Leave\n");
  printf("Opcao:\n");
  scanf("%d",&op);
  return(op);
@@ -40,15 +50,15 @@ int main()
                     printf("Client's Balance: \n");
                     scanf("%f", &bal);
                     printf("Client's Name: \n");
-                    scanf("%[^\n]", name);
+                    scanf("%s", name);
                     printf("Client's Address: \n");
-                    scanf("%[^\n]", addr);
+                    scanf("%s", addr);
                     clients = insertClient(clients, nif, bal, name, addr);
                 break;
             case 2: printf("Vehicle's Code: \n");
                     scanf("%d", &cod);
                     printf("Vehicle's Type: \n");
-                    scanf("%[^\n]", tp);
+                    scanf("%s", tp);
                     printf("Vehicle's Battery: \n");
                     scanf("%d", &bat);
                     printf("Vehicle's Price: \n");
@@ -58,7 +68,7 @@ int main()
             case 3: printf("Manager's Code: \n");
                     scanf("%d", &mcod);
                     printf("Manager's Name: \n");
-                    scanf("%[^\n]", mname);
+                    scanf("%s", mname);
                     managers = insertManager(managers, mcod, mname);
                 break;
             case 4: saveClient(clients);    break;
@@ -67,6 +77,9 @@ int main()
             case 7: clients = readClient();     break;
             case 8: vehicles = readVehicle();   break;
             case 9: managers = readManager();   break;
+            case 10:   break;
+            case 11:   break;
+            case 12:   break;
             default:
                 break;
         }
